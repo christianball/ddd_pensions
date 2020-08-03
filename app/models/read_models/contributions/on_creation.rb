8b2bf570aa993badeef48fa40d16a9caa08ea0f0
Christian Ball <christianball@hotmail.co.uk>
2020-08-03 08:13:26 +0100

    Add OnCreation event handler in read model.

--- app/models/read_models/contributions/on_creation.rb
+++ app/models/read_models/contributions/on_creation.rb
@@ -1,5 +1,23 @@
 module ReadModels
   module Contributions
+    class OnCreation
 
+      def call(event)
+        contribution_from(event).save!
+      end
+
+      private
+
+      def contribution_from(event)
+        Contribution.new(
+          employee_name: event.data[:employee_name],
+          starts_on: event.data[:starts_on],
+          ends_on: event.data[:ends_on],
+          amount: event.data[:amount],
+          currency: event.data[:currency]
+        )
+      end
+
+    end
   end
 end
